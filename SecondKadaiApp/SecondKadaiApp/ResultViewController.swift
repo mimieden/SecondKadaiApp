d2648ade71821173cb310f049ea5632434a0bf1b
mimieden <mimieden525@gmail.com>
2017-06-14 22:37:01 +0900

    rev 1.0

---
+++ SecondKadaiApp/SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by mimieden on 2017/06/14.
+//  Copyright © 2017年 mimieden. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    // 2画面目のLabelを、StoryboardでこのViewControllerにIBOutletとして接続しておく
+    @IBOutlet weak var lavel: UILabel!
+    
+    // 受け取るためのプロパティ（変数）を宣言しておく
+    var name_input:String = ""
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let name = name_input
+        lavel.text = "こんにちは、\(name)さん"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/SecondKadaiApp/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  FirstKadaiApp
+//  SecondKadaiApp
 //
-//  Created by mimieden on 2017/06/13.
+//  Created by mimieden on 2017/06/14.
 //  Copyright © 2017年 mimieden. All rights reserved.
 //
 
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //ユーザーの入力値を取得する
+    @IBOutlet weak var inputName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,13 +26,14 @@ class ViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // segueから遷移先のResultViewControllerを取得する
         let resultViewController:ResultViewController = segue.destination as! ResultViewController
-        // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
-        resultViewController.x = 1
-        resultViewController.y = 1
+        // 遷移先のResultViewControllerでnameに値を代入して渡す
+        resultViewController.name_input = inputName.text!
     }
     
-    @IBAction func unwind(segue: UIStoryboardSegue) {
-        // 他の画面から segue を使って戻ってきた時に呼ばれる
-    }
+        @IBAction func unwind(segue: UIStoryboardSegue) {
+            //他の画面からsequeを使って戻ってきた時によばれる
+        }
+
+
 }
 
